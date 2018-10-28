95f21621d2099fb9930261200b7615e224ba1417
Liqingfeng <562916584@qq.com>
2018-10-28 23:49:11 +0800

    分布式 开始

--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -1,6 +1,11 @@
 package persist
 
-import "log"
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"gopkg.in/olivere/elastic.v5"
+	"log"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -10,7 +15,26 @@ func ItemSaver() chan interface{} {
 			item := <-out
 			log.Printf("Saver item :#%d:  %v\n", itemCount, item)
 			itemCount++
+
+			save(item)
 		}
 	}()
 	return out
+}
+
+func save(item interface{}) {
+	client, err := elastic.NewClient(
+		// 运行在docker上 是内网访问
+		elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Index().Index("dating_profile").
+		Type("zhenai").BodyJson(item).
+		Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("%+v", resp)
 }
