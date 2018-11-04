35ca3b8bcf612e28ccfc5480efa7d8ba8e542680
Liqingfeng <562916584@qq.com>
2018-11-04 21:56:18 +0800

    爬虫

---
+++ crawler/fronted/model/page.go
@@ -0,0 +1,13 @@
+package model
+
+type SearchResult struct {
+	Hits        int64
+	Start       int
+	Query       string
+	PrevFrom    int
+	NextFrom    int
+	CurrentPage int
+	TotalPage   int64
+	Items       []interface{}
+	//Items []engine.Item
+}
