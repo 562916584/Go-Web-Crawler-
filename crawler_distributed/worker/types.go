3c4e9f6f8fb02f4bc76f7930abcb8ce36b2e4800
Liqingfeng <562916584@qq.com>
2018-11-09 18:46:56 +0800

    实现 函数序列化

--- crawler_distributed/worker/types.go
+++ crawler_distributed/worker/types.go
@@ -9,65 +9,65 @@ type SerializedParser struct {
 	// 函数参数
 	Args interface{}
 }
+
 // {'ParseCity',nil}  {'ParseCityList',nil} {'ProfileParser',Username}
 type Request struct {
-	Url  string
+	Url    string
 	Parser SerializedParser
 }
 type ParseResult struct {
-	Items []engine.Item
+	Items    []engine.Item
 	Requests []Request
 }
+
 // 将request序列化
-func SerialzeRequest(r engine.Request) Request  {
+func SerialzeRequest(r engine.Request) Request {
 	name, args := r.Parser.Serialize()
 	return Request{
-		Url:r.Url,
+		Url: r.Url,
 		Parser: SerializedParser{
-			Name:name,
-			Args:args,
+			Name: name,
+			Args: args,
 		},
 	}
 }
+
 // 将ParseResult 序列化
-func SerialzeResult(r engine.ParseResult) ParseResult  {
+func SerialzeResult(r engine.ParseResult) ParseResult {
 	result := ParseResult{
-		Items:r.Items,
+		Items: r.Items,
 	}
 	for _, req := range r.Requests {
-		result.Requests=append(result.Requests,
+		result.Requests = append(result.Requests,
 			SerialzeRequest(req))
 	}
 	return result
 }
+
 // 将序列化Request 转化为engine的request
 func DeserializeRequest(r Request) engine.Request {
 	return engine.Request{
-		Url:r.Url,
-		Parser:deserializeParser(r.Parser),
+		Url:    r.Url,
+		Parser: deserializeParser(r.Parser),
 	}
 }
 func deserializeParser(p SerializedParser) engine.Parser {
-	switch p.Name {
-	case :
-		
-	}
 }
-func DeserializeResult( r ParseResult) engine.ParseResult {
+func DeserializeResult(r ParseResult) engine.ParseResult {
 	result := engine.ParseResult{
-		Items:r.Items,
+		Items: r.Items,
 	}
 	for _, req := range r.Requests {
-		result.Requests=append(result.Requests,
+		result.Requests = append(result.Requests,
 			DeserializeRequest(req))
 	}
 	return result
 }
-type CrawlService struct {
 
+type CrawlService struct {
 }
 
 func (CrawlService) Procress(
 	req engine.Request, result *engine.ParseResult) error {
-
+	panic("aaa")
 }
